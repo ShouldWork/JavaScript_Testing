bb1815254e980e5fbffbf4ffb3f86eac5fd1c6ba
Krysp <mrblocke@gmail.com>
2016-06-08 11:01:03 -0600

    Updated totals for in and out. Updated total column. Updated anti-cheat

--- js/linkedlist.js
+++ js/linkedlist.js
@@ -1,3 +1,31 @@
 /**
  * Created by Krysp on 6/7/16.
  */
+function LinkedList() {
+    this.head = null; 
+}
+
+LinkedList.prototype.push = function(val) {
+    var node = {
+        value: val,
+        next: null
+    };
+    if (!this.head) {
+        this.head = node;
+    }
+    else {
+        current = this.head;
+        while(current.next){
+            current = current.next;
+        }
+        current.next = node;
+    }
+};
+
+var myll = new LinkedList();
+
+myll.push(2);
+myll.push(10);
+myll.push(9);
+
+console.log(myll);
--- js/scoreCard.js
+++ js/scoreCard.js
@@ -1,59 +1,3 @@
-/**
- * Created by Krysp on 5/18/16.
-
- local_obj = {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            radius: 100
-        }
- get_course(local_obj);
- console.log("Locations: " + local_obj.latitude);
-
-
-
-
-
-
-
- if ("geolocation" in navigator) {
- getCoords();
- console.log('I have the navigator option!')
- } else {
- alert("This doesn't exist!")
- }
-
- function getCoords() {
- console.log('Getting coords');
- navigator.geolocation.getCurrentPosition(function(position) {
- local_obj = {
- latitude: 40.4426135,
- longitude: -111.8631116,
- radius: 100
- }
- show_cords(position.coords.latitude, position.coords.longitude);
-
- })
- }
-
-
- function show_cords(lat,lon) {
- console.log("Latitude position: " + lat + "\nLongitude position: " + lon);
- };
-
- function get_courses(obj) {
- xhttpPost.onreadystatechange = function () {
- if (xhttpPost.readyState == 4 && xhttpPost.status == 200) {
- console.log(xhttpPost.responseText);
- var closeCourses = JSON.parse(xhttpPost.responseText);
- console.log(closeCourses);
- }
- };
- xhttpPost.open("POST","https://golf-courses-api.herokuapp.com/courses",true);
- xhttpPost.send(obj)
- };
-
- */
-
 var testCourse ={};
 var closeCourses={};
 var xhttp = new XMLHttpRequest();
@@ -76,20 +20,66 @@ $(document).ready(function() {
             }
             $("#doDiv").show();
         },
-         error: function (request,errorType,errorMessage){
+        error: function (request,errorType,errorMessage){
             $("#selectCourse").append(errorMessage);
-         },
-         timeout: 3000,
-         beforeSend: function(){
+        },
+        timeout: 3000,
+        beforeSend: function(){
             $("#selectCourse").append("<img src='../src/images/svg/golfClub.svg' class='is-loading' id='golfLoading'>");
-            $("#golfLoading").animate({"transform": "rotate(360deg"},'fast');
-         },
-         complete: function(){
+            $("#golfLoading").animate({"transform": "rotate(360deg)"},'fast');
+        },
+        complete: function(){
             $("#golfLoading").remove();
-         },
-         data: {"latitude": 40.4426135,"longitude": -111.8631116,"radius": 100}
+        },
+        data: {"latitude": 40.4426135,"longitude": -111.8631116,"radius": 100}
     });
 });
+
+
+$(document).ready(function(){
+    var scoreCard = $("#scoreCard");
+    scoreCard.on('focus','.scoreParContainer',function() {
+        currentColor = $(this).css('border-color');
+        $(this).css({"border-color": "rgba(0,255,0,0.90)"});
+    });
+    scoreCard.on('focusout','.scoreParContainer',function() {
+        $(this).css({"border-color": currentColor});
+    });
+});
+
+$(document).ready(function(){
+    var currentColor,
+        scoreCard = $("#scoreCard"),
+        outRow = '',
+        player ='',
+        backOutRow;
+    scoreCard.on('change','.holeScoreRow',function(){
+        outRow = this.id.substr(0,7) + "out";
+        backOutRow = this.id.substr(0,7) + "backOut";
+        var score = +$(this).val();
+        if ($(this).data("edited") == true){
+            if ($(this).val() != ''){
+                $(this).css("background-color", "rgba(255,0,0,0.60)")
+            }
+        } else {
+            $(this).attr("data-edited","true");
+        }
+        if (this.id.length <= 16){
+            player = this.id.substr(0,this.id.length - 1);
+            var col = Number(this.id.substr(this.id.length - 1));
+            if (col <=8){
+                updateIn(player,outRow);
+            } else {
+                updateOut(player,backOutRow);
+            }
+        } else {
+            player = this.id.substr(0,this.id.length - 2);
+            updateOut(player,backOutRow);
+        }
+
+    });
+});
+
 
 var numHoles;
 function getCourseInfo(id) {
@@ -109,49 +99,56 @@ function buildPage(numHoles) {
     "use strict";
     vertText("Map");
     var scC = $("#scoreCard");
-    $("#slideButton").css("display","block");
-    $("#slideWeather").css("display","block").hover( function() {
+    $("#slideButton").show();
+    $("#slideWeather").show().hover( function() {
         $("#weatherContainer").slideToggle();
     });
     $("#courseContainer").slideUp();
-    $("#courseStats").css("display","block").hover( function() {
+    $("#courseStats").show().hover( function() {
         $("#courseContainer").slideToggle();
     }).on('click',courseStats());
     //getMyInfo(testCourse.course.zip_code.substr(0,5));
     getMyInfo();
     $("#selectCourse").remove();
     //$("#doDiv").css("display","none");
-    $("#addPlayerBtn").css("display","block");
+    $("#addPlayerBtn").show();
     $("#courseName").html(testCourse.course.name);
     $("#newCourse").css("display","block");
     $("#theCity").append(testCourse.course.city + ", " + testCourse.course.state_or_province);
     $("#phoneNumber").append(testCourse.course.phone);
     $("#site a").attr("href",testCourse.course.website).text(testCourse.course.name);
 
-    var playerCol = "<div id='playrCol' class='playerCol'></div>";
-        scC.append(playerCol);
+    var playerCol = "<div id='playerCol' class='playerCol'></div>";
+    scC.append(playerCol);
     var holeRowTitle = "<div class='playerCell'>Hole</div>";
-        $("#playerCol").append(holeRowTitle);
+    $("#playerCol").append(holeRowTitle);
     var parRowTitle = "<span class='playerCell'>Par</span>";
-        $("#playerCol").append(parRowTitle);
+    $("#playerCol").append(parRowTitle);
     for (var j = 0; j < numHoles; j++ ) {
+        var hole = j + 1;
         if (j == 9){
-            var outCell = "<div class='outCell'>Out</div>",
+            var outCell = "<div class='outCell'>In</div>",
                 outCol = "<div class='holeCol' id='outCol'></div>",
-                blankOutCell = "<div id='blankOut' class='outCell'>31</div>";
+                blankOutCell = "<div id='blankOut' class='outCell'></div>";
             scC.append(outCol);
             $("#outCol").append(outCell).append(blankOutCell);
         }
-        var holeColTitleRow = "<span class='holeColTitleRow'>" + j + "</span>",
+        var holeColTitleRow = "<span class='holeColTitleRow'>" + hole + "</span>",
             holeColId = 'column' + j,
             holeColParRow = "<span class='parRowPro'>" + testCourse.course.holes[j].tee_boxes[0].par + "</span>",
             holeCol = "<div id='" + holeColId + "' class='holeCol'></div>";
         scC.append(holeCol);
         $("#" + holeColId).append(holeColTitleRow).append(holeColParRow);
     }
-    var totalCol = "<div id='totalColumn' class='holeCol'></div>";
-    $("#totalColumn").append(outCell);
-    scC.append(totalCol);
+    var totalCol = "<div id='totalColumn' class='holeCol'></div>",
+        totalCell = "<div class='outCell'></div>",
+        totalTitle = "<div class='outCell'>Total</div>",
+        backOutCell = "<div class='outCell'></div>",
+        backOutCol = "<div id='backOutCol' class='holeCol'></div>",
+        backOutTitle = "<div class='outCell'>Out</div>";
+    scC.append(backOutCol).append(totalCol);
+    $("#backOutCol").append(backOutTitle).append(backOutCell);
+    $("#totalColumn").append(totalTitle).append(totalCell);
     setTimeout(function () {
             getMapCoord();
         },250
@@ -178,14 +175,15 @@ function newCourse() {
 
 function addPlayer(){
     numPlayers ++;
-    var level = $("#teeDrop").val(); //document.getElementById('teeDrop').value ,
+    var totalCell = "<div class='outCell' id='player" + numPlayers + "total'>0</div>";
+    backOut = "<div class='outCell'>31</div>";
+    var level = $("#teeDrop").val(),
+        newName = $("#playerName").val(),
+        handicap = $("#handicap").val();
     players.push({name: newName, level: level, handicap: handicap});
     var thisPlayerOut = "player" + numPlayers + "out",
         thisPlayer = "player" + numPlayers,
-        newName = $("#playerName").val(),  // document.getElementById('playerName').value,
-        handicap = $("#handicap").val(), //document.getElementById('handicap').value,
         playerName ="<span class='playerCell'>" + players[numPlayers -1].name + "</span>";
-
     $("#playerCol").append(playerName);
     for (var k = 0; k < testCourse.course.hole_count; k++) {
         if (k == 9){
@@ -199,42 +197,63 @@ function addPlayer(){
             scoreParContainer = "<div class='scoreParContainer' id='" + scoreParContainerId + "'></div>",
             holeScoreRowId = 'player' + numPlayers + 'scoreCol' + k,
             teeColor = testCourse.course.holes[k].tee_boxes[players[numPlayers - 1].level].tee_hex_color,
-            holeScoreRow = "<input class='holeScoreRow' id='" + holeScoreRowId + "'>",
-            teeType = testCourse.course.holes[k].tee_boxes[players[numPlayers - 1].level].tee_type;
-        console.log(teeColor);
+            teeType = testCourse.course.holes[k].tee_boxes[players[numPlayers - 1].level].tee_type,
+            backOutCell = "<div id='" + playerOut + "backOut' class='outCell'>0</div>";
         $("#column" + k).append(scoreParContainer);
-        if (testCourse.course.holes[k].tee_boxes[players[numPlayers - 1].level].tee_type == 'men') {
-            $("#" + scoreParContainerId).css({"color": "black", "border": "solid 2px black"});
-        } else {
-            $("#" + scoreParContainerId).css({"color": "white", "border": "solid 2px ghostWhite"});
-        }
-        $("#" + scoreParContainerId).css("background-color",teeColor);
-       // scoreParContainer.style.background = testCourse.course.holes[k].tee_boxes[players[numPlayers - 1].level].tee_hex_color;
-        $("#" + scoreParContainerId).append(playerLevelPar).append(holeScoreRow);
-        $("#" + holeScoreRow).attr("onkeyup","update()");
+        $("#" + scoreParContainerId).append(playerLevelPar).append("<input class='holeScoreRow' id='" + holeScoreRowId + "'>");
         if (teeType == 'men') {
-            $("#" + holeScoreRowId).css({"color": "black","border": "solid 2px white"});
+            $("#" + scoreParContainerId).css({"color": "black", "border": "solid 2px black","background-color": teeColor});
+            $("#" + holeScoreRowId).css({"color": "black","border-left": "solid 2px black"});
         } else {
-            $("#" + holeScoreRowId).css({"color": "ghostWhite","border": "solid 2px black"});
+            $("#" + scoreParContainerId).css({"color": "white", "border": "solid 2px ghostWhite","background-color": teeColor});
+            $("#" + holeScoreRowId).css({"color": "ghostWhite","border-left": "solid 2px ghostWhite"});
         }
     }
+    $("#totalColumn").append(totalCell);
+    $("#backOutCol").append(backOutCell);
     $("#playerName").val('');
     $("#teeDrop").html('');
     $("#handicap").val('');
 }
 
-function updateOut(player,row) {
+function updateIn(player,row) {
     "use strict";
     var outScore = 0;
-    for (var pt = 0; pt < numHoles; pt++) {
-        var ptc = player + "scoreCol" + pt;
-        var score = Number(document.getElementById(ptc).value);
-        if (score != '') {
+    for (var pt = 0; pt < numHoles / 2; pt++) {
+        var score = +$("#" + player + pt).val();
+        if (score !== ''){
             outScore += score;
         }
     }
-    document.getElementById(row).innerHTML = outScore;
-};
+    $("#" + row).html(outScore);
+    updateTotal(player.substr(0,7) + "total",player);
+}
+
+function updateOut(player,row) {
+    "use strict";
+    var outScore = 0;
+    for (var pt = 9; pt < numHoles; pt++) {
+        var score = +$("#" + player + pt).val();
+        if (score !== ''){
+            outScore += score;
+        }
+    }
+    $("#" + row).html(outScore);
+    updateTotal(player.substr(0,7) + "total",player);
+}
+
+function updateTotal(playerTotal,playerCol){
+    "use strict";
+    console.log(playerCol);
+    var totalScore = 0;
+    for (var tt = 0; tt < numHoles; tt++) {
+        var score = +$("#" + playerCol + tt).val();
+        if (score !== ''){
+             totalScore += score;
+        }
+    }
+    $("#" + playerTotal).html(totalScore);
+}
 
 function toCapitalize(v) {
     "use strict";
@@ -253,20 +272,14 @@ function vertText(textVert){
 function slideBtn() {
     secondSlidePos = $('#secondSlide').position();
     if (secondSlidePos.left > 0) {
-        document.getElementById("slideButton").style.paddingTop = '20px';
-        document.getElementById("slideButton").style.height = "75%";
-        document.getElementById("firstSlide").style.left = "100%";
-        document.getElementById("firstSlide").style.opacity = "0";
-        document.getElementById("secondSlide").style.left = "0";
-        document.getElementById("secondSlide").style.opacity ="1";
+        $("#slideButton").css({"padding-top": '20px',"height": "75%"});
+        $("#firstSlide").css({"left": "100%", "opacity": "0"});
+        $("#secondSlide").css({"left": "0","opacity": "1"});
         vertText("Course");
     } else {
-        document.getElementById("slideButton").style.paddingTop = '50px';
-        document.getElementById("slideButton").style.height = '100%';
-        document.getElementById("firstSlide").style.left = '0';
-        document.getElementById("secondSlide").style.opacity ="0";
-        document.getElementById("secondSlide").style.left = "100%";
-        document.getElementById("firstSlide").style.opacity = "1";
+        $("#slideButton").css({"padding-top": '50px',"height": "100%"});
+        $("#firstSlide").css({"left": "0", "opacity": "1"});
+        $("#secondSlide").css({"left": "100%","opacity": "0"});
         vertText("Map");
     }
 }
@@ -299,7 +312,7 @@ function getMapCoord() {
         holeLocations.push({greenLat: testCourse.course.holes[g].green_location.lat, greenLng: testCourse.course.holes[g].green_location.lng,teeLat: testCourse.course.holes[g].tee_boxes[0].location.lat,teeLng: testCourse.course.holes[g].tee_boxes[0].location.lng});
         //  console.log(holeLocations[g].lat + " & " + holeLocations[g].lng);
         document.getElementById("column" + g).setAttribute('onclick','reCenterMap(' + holeLocations[g].greenLat + ',' + holeLocations[g].greenLng + ')');
-        console.log("Green: " + holeLocations[g].greenLat + " & " + holeLocations[g].greenLng + "\nTee: " + holeLocations[g].teeLat + " & " + holeLocations[g].teeLng);
+        //console.log("Green: " + holeLocations[g].greenLat + " & " + holeLocations[g].greenLng + "\nTee: " + holeLocations[g].teeLat + " & " + holeLocations[g].teeLng);
 
     }
     var courseLat = testCourse.course.location.lat;
@@ -320,10 +333,6 @@ function reCenterMap(lat,lng) {
 }
 
 
-
-//Weather Stuff
-
-//Weather Stuff
 
 //weather API Call
 var xhttpWeather;
@@ -363,7 +372,7 @@ function getMyInfo() {
             $("#humidity").append(response.main.humidity);
             $("#temp").append(temperature.toFixed(0));
             $("#temp img").first().attr("src","../src/images/svg/farenheit.svg");
-            $("#wind").append(windSpeed);
+            $("#wind").append(windSpeed).append("<img src''>");
             if (windSpeed > 0 && windSpeed <= 12 ) {
                 $("#wind img").attr('src','../src/images/svg/wind-3.svg')
             } else if (windSpeed > 12 && windSpeed <= 18) {
@@ -401,49 +410,3 @@ function newZip () {
     var thisZip = $("#zipCode").val();
     getMyInfo(thisZip);
 };
-/*
- //weather API Call
- var xhttpWeather;
- var weatherObject = {};
- var weatherIcon = "../src/images/svg/";
- $(document).ready(function () {
- $("#slideWeather").on('click',function() {
- "use strict";
- $("#weatherContainer").slideToggle('fast',"swing");
- });
- });
-
- function getMyInfo() {
- var zipCode = $("#zipCode").val();
- $.ajax('http://api.openweathermap.org/data/2.5/weather', {
- success: function(response) {
- clearWeather();
- $("#cityName").append("<h1>" + response.name + "</h1>");
- $("#temp").append("<span>" + Math.ceil(response.main.temp) + "</span>");
- $("#temp img").attr("src","../src/images/svg/farenheit.svg");
- $("#wind").html("<span>" + response.wind.speed + "<span>");
- $("#cityName").append("<h4>" + response.weather[0].description + "</h4>");
- $("#icon img").first().attr("src", weatherIcon + response.weather[0].icon + ".svg");
- },
- data: {"zip": zipCode,"appid": "a4e12bc54b22227bd03bb03c867242d7","units": "imperial"},
- error: function (request,errorType,errorMessage){
- $("#cityName").append(errorMessage);
- },
- timeout: 3000,
- beforeSend: function(){
- $("#slideWeather").css({"background-color": "red","color": "black"})
- },
- complete: function(){
- $("#slideWeather").css({"background-color": "rgba(0,0,0,0.80)", "color": "ghostWhite"})
- }
- });
- }
-
-
- function clearWeather() {
- "use strict";
- $("#weatherContainer > div").not('#icon').html('');
- $('#temp img').append('img src="../src/images/svg/farenheit.svg"');
- $("#icon img").attr('src','');
- }
- */
